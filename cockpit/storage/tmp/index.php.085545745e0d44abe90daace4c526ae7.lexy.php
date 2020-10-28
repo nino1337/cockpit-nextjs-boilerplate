477caa8815513724fc6ef76786bde720d5c16b5c
Nino Dornemann <nino.dornemann@urbanenergy.de>
2020-10-28 09:51:44 +0100

    Update: [No-Issue] remove tmp files from repo

--- cockpit/storage/tmp/index.php.085545745e0d44abe90daace4c526ae7.lexy.php
+++
@@ -1,9 +0,0 @@
-<div>
-    <ul class="uk-breadcrumb">
-        <li class="uk-active"><span><?php echo $app("i18n")->get('Assets'); ?></span></li>
-    </ul>
-</div>
-
-<div riot-view>
-    <cp-assets></cp-assets>
-</div>
